d6c56e23c2546796ff7396ea569e227e9abe2f11
TaniaLapsh <1tatianalapshina@gmail.com>
2019-12-24 21:28:57 -0500

    inst the class in another

---
+++ controllerTest.py
@@ -0,0 +1,21 @@
+from PySide2 import QtCore, QtGui, QtWidgets
+
+import controllerTestUI as ui
+reload(ui)
+
+class ControllerTest(QtWidgets.QMainWindow):
+    def __init__(self, parent = None):    
+        super(ControllerTest, self).__init__(parent) 
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        
+        self.ui = ui.ControllerTestUI()
+        self.ui.create_window(self)
+        
+
+if __name__ == '__main__':
+    
+    app = QtWidgets.QApplication([])  
+    win = ControllerTest()   
+    win.show()
+    app.exec_()
+
--- controllerTestUI.py
+++ controllerTestUI.py
@@ -1,16 +1,11 @@
 from PySide2 import QtCore, QtGui, QtWidgets
 
-class ControllerTest(QtWidgets.QMainWindow):
-    def __init__(self, parent = None):
-        
-        super(ControllerTest, self).__init__(parent) 
-        self.__create_window()   
-        
-    def __create_window(self):
-        self.mainWindow = self # Main Window
-        self.centralWidget1 = QtWidgets.QWidget(self.mainWindow) #central widget1
+class ControllerTestUI(object):        
+    def create_window(self, MainWindow):
+        MainWindow.setObjectName('MainWindow') # Main Window
+        self.centralWidget1 = QtWidgets.QWidget(MainWindow) #central widget1
         self.widget1 = QtWidgets.QWidget(self.centralWidget1) #widget1
-        self.shapeNodesComboBox = QtWidgets.QComboBox(self.widget1) #comboBox
+        self.shapeNodesListWidget = QtWidgets.QListWidget(self.widget1) #comboBox
         self.label1 = QtWidgets.QLabel(self.widget1)
         self.label1.setText('Object')
 
@@ -23,15 +18,9 @@ class ControllerTest(QtWidgets.QMainWindow):
         self.horizontalLayout1 = QtWidgets.QHBoxLayout(self.widget1)
         self.widget1.setLayout(self.horizontalLayout1)
         self.horizontalLayout1.addWidget(self.label1)
-        self.horizontalLayout1.addWidget(self.shapeNodesComboBox)
+        self.horizontalLayout1.addWidget(self.shapeNodesListWidget)
 
         #set central widget for mainWindow
-        self.mainWindow.setCentralWidget(self.centralWidget1)
-
-if __name__ == '__main__':
-    
-    app = QtWidgets.QApplication([])  
-    win = ControllerTest()   
-    win.show()
-    app.exec_()
+        MainWindow.setCentralWidget(self.centralWidget1)
+   
 
