4c90971c9033cae24877b5b5db111d8696fc00b2
TaniaLapsh <1tatianalapshina@gmail.com>
2019-12-30 11:33:47 -0500

    WIP

--- controllerTest.py
+++ controllerTest.py
@@ -6,7 +6,7 @@ reload(ui)
 
 class ControllerTest(QtWidgets.QMainWindow):
 
-    itemClicked = QtCore.Signal(list)
+    itemClicked = QtCore.Signal(list,list)
 
     def __init__(self, parent = None):    
         super(ControllerTest, self).__init__(parent) 
@@ -31,26 +31,37 @@ class ControllerTest(QtWidgets.QMainWindow):
         self.shapeNodes = pm.ls(exactType = 'mesh')
         for obj in self.shapeNodes:
             self.ui.shapeNodesListWidget.addItem(str(obj))
-            self.listItems.append(obj)
-            
+            self.listItems.append(obj)          
     
-    #(1.2)signal object clicked is emited with list argument = 
+    #(1.2)signal object clicked is emited with list argument 
     def _onItemClicked(self):
         self.selectedItems = self.ui.shapeNodesListWidget.selectedItems()
-        self.itemClicked.emit(self.selectedItems)
-       # print (selectedItemsNames)
-
+        self.itemClicked.emit(self.listItems,self.selectedItems)
 
     def _onSelectionUpdate(self):
-        mayaSelectedObjects = pm.ls(selection = True)
+        selectedNames = []
+        allItems = []
+
         for item in self.selectedItems:
-            print (item.text())
-            if item.text() in mayaSelectedObjects:
-                pass
-            elif item.text() not in mayaSelectedObjects:
-                pm.select(item.text(), add = True)
-        notSelectedItems =             
-            
+            selectedName = item.text()
+            selectedNames.append(selectedName)
+        for item in self.listItems:
+            itemName = item.shortName()
+            allItems.append(itemName)
+        notSelected =[item for item in allItems if item not in selectedNames]
+        print ('selected Items: {0}'.format(selectedNames))
+        print ('all  Items are: {0}'.format(allItems))
+        print ('not  selected : {0}\n'.format(notSelected))
+
+        for item in allItems:
+            if item in selectedNames:
+                pm.select(item, add = True)
+            elif item in notSelected:
+                pm.select(item, deselect = True)
+            else:
+                print ('Error!!! with selection')
+
+
 if __name__ == '__main__':
     
     app = QtWidgets.QApplication([])  
