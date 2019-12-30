bbf2bf15dcdb9823ca9abaa67529619fbcd88cf4
TaniaLapsh <1tatianalapshina@gmail.com>
2019-12-30 13:40:53 -0500

    maya callback created

--- controllerTestMaya.py
+++ controllerTestMaya.py
@@ -1,17 +1,27 @@
 from PySide2 import QtWidgets, QtGui, QtCore
 import maya.OpenMayaUI as OpenMayaUI
+import maya.OpenMaya as OpenMaya
 from shiboken2 import wrapInstance 	
+import pymel.core as pm
 
 import controllerTest
 reload(controllerTest)
+
+class ControllerTestMaya(QtCore.QObject):
+    objectSelected = QtCore.Signal(list)
 
 window = None
 def show():
     global window
     if window is None:
+        cont = ControllerTestMaya()
         parent = get_maya_window()
         window = controllerTest.ControllerTest(parent)
 
+        def emit_selchanged(_):
+            cont.objectSelected.emit(pm.selected(type = 'mesh'))
+            print ('Signal Emited From Maya')
+        OpenMaya.MEventMessage.addEventCallback('SelectionChanged', emit_selchanged)
     window.show()
 
 def get_maya_window():
